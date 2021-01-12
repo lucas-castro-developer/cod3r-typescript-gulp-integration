2693694f240f30ea6ab319f23497055935f4103d
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-12 15:31:52 -0300

    Generating production javascript

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const del = require("del");
 const browserify = require("browserify");
 const source = require("vinyl-source-stream");
 const tsify = require("tsify");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
 
 function limparDist() {
   return del(["dist"]);
@@ -14,11 +16,24 @@ function copiarHTML() {
 
 function gerarJS() {
   return browserify({
-      basedir: '.',
-      entries: ['src/main.ts']
-  }).plugin(tsify)
-  .bundle()
-  .pipe(source('app.js')).pipe(dest('dist'))
+    basedir: ".",
+    entries: ["src/main.ts"],
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(dest("dist"));
 }
 
-exports.default = series(limparDist, parallel(gerarJS, copiarHTML));
+function gerarJSProducao() {
+  return src("dist/app.js")
+    .pipe(rename("app.min.js"))
+    .pipe(uglify())
+    .pipe(dest("dist"));
+}
+
+exports.default = series(
+  limparDist,
+  parallel(gerarJS, copiarHTML),
+  gerarJSProducao
+);
