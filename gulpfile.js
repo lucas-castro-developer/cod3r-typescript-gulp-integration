a711921aa7f5021bca75bf63ef0a5767cd8ae58d
Lucas Castro <lucas.sc98@hotmail.com>
2021-01-12 15:10:44 -0300

    Implementing functions used on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
 const { series, parallel, src, dest } = require("gulp");
 const del = require("del");
 const browserify = require("browserify");
-const source = require('vinyl-source-stream')
-const tsify = require('tsify')
+const source = require("vinyl-source-stream");
+const tsify = require("tsify");
 
-function limparDist(cb) {
-    cb()
+function limparDist() {
+  return del(["dist"]);
 }
 
-function copiarHTML(cb) {
-    cb()
+function copiarHTML() {
+  return src("public/**/*").pipe(dest("dist"));
 }
 
-function gerarJS(cb) {
-    cb()
+function gerarJS() {
+  return browserify({
+      basedir: '.',
+      entries: ['src/main.ts']
+  }).plugin(tsify)
+  .bundle()
+  .pipe(source('app.js')).pipe(dest('dist'))
 }
 
-exports.default = series(
-    limparDist,
-    parallel(gerarJS, copiarHTML)
-)
+exports.default = series(limparDist, parallel(gerarJS, copiarHTML));
